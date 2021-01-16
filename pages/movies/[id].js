56edff62fa31593b8cd8a8c81ab5a0d28d7f16cd
Jack Scott <jack.scott10@gmail.com>
2021-01-16 20:09:16 +0000

    Fix details table

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -42,21 +42,119 @@ export default function MoviePage({ details }) {
                   </p>
                   <p class="subtitle is-5 has-text-white">Viewer Rating</p>
                 </div>
+
                 <div class="block content is-small">
+                  <div class="table-container">
+                    <table class="table is-narrow has-background-dark">
+                      <tr>
+                        <td>
+                          <p class="has-text-white">Genre: </p>
+                        </td>
+                        <td>
+                          {details.genres.map((c) => (
+                            <div class="info-tag">{c.name}</div>
+                          ))}
+                        </td>
+                      </tr>
+
+                      <tr>
+                        <td>
+                          <p class="has-text-white">Runtime: </p>
+                        </td>
+                        <td>
+                          <p class="has-text-white">
+                            {details.runtime} minutes
+                          </p>
+                        </td>
+                      </tr>
+
+                      <tr>
+                        <td>
+                          <p class="has-text-white">Country: </p>
+                        </td>
+                        <td>
+                          {details.production_countries.map((c) => (
+                            <div class="info-tag">{c.name}</div>
+                          ))}
+                        </td>
+                      </tr>
+
+                      <tr>
+                        <td>
+                          <p class="has-text-white">Produced by: </p>
+                        </td>
+                        <td>
+                          <div
+                            style={{ "flex-wrap": "wrap" }}
+                            class="is-inline-flex"
+                          >
+                            {details.production_companies.map((c) => (
+                              <div class="info-tag">{c.name}</div>
+                            ))}
+                          </div>
+                        </td>
+                      </tr>
+
+                      {details.budget ? (
+                        <tr>
+                          <td>
+                            <p class="has-text-white">Budget: </p>
+                          </td>
+                          <td>
+                            <p class="has-text-white">
+                              {new Intl.NumberFormat("en-US", {
+                                style: "currency",
+                                currency: "USD",
+                              }).format(details.budget)}
+                            </p>
+                          </td>
+                        </tr>
+                      ) : null}
+
+                      {details.revenue ? (
+                        <tr>
+                          <td>
+                            <p class="has-text-white">Revenue: </p>
+                          </td>
+                          <td>
+                            <p class="has-text-white">
+                              {new Intl.NumberFormat("en-US", {
+                                style: "currency",
+                                currency: "USD",
+                              }).format(details.revenue)}
+                            </p>
+                          </td>
+                        </tr>
+                      ) : null}
+                    </table>
+                  </div>
+                </div>
+                {/* <div class="block content is-small">
                   <p class="has-text-white">
-                    Genre: {details.genres.map((c) => `${c.name} `)}
+                    Genre:{" "}
+                    {details.genres.map((c) => (
+                      <div class="info-tag">{c.name}</div>
+                    ))}
                   </p>
                   <p class="has-text-white">
                     Runtime: {details.runtime} minutes
                   </p>
                   <p class="has-text-white">
                     Country:{" "}
-                    {details.production_countries.map((c) => `${c.name} `)}
+                    {details.production_countries.map((c) => (
+                      <div class="info-tag">{c.name}</div>
+                    ))}
                   </p>
-                  <p class="has-text-white">
-                    Produced By:{" "}
-                    {details.production_companies.map((c) => `${c.name} `)}
-                  </p>
+                  <div>
+                    <p style={{ float: "left" }} class="has-text-white">
+                      Produced By:{" "}
+                    </p>
+                    <div class="">
+                      {details.production_companies.map((c) => (
+                        <div class="info-tag">{c.name}</div>
+                      ))}
+                    </div>
+                  </div>
                   {details.budget ? (
                     <p class="has-text-white">
                       Budget:{" "}
@@ -75,7 +173,7 @@ export default function MoviePage({ details }) {
                       }).format(details.revenue)}
                     </p>
                   ) : null}
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
