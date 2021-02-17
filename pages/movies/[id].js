39b5ec48d1fba6dbe168be122030435e2cbd7c34
Jack Scott <jack.scott10@gmail.com>
2021-02-17 21:38:53 +0000

    Add sharing meta

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import Head from "next/head";
 import Detail from "../../components/detail";
-import MovieCard from "../../components/movie-card";
+import MovieCard, { getPath } from "../../components/movie-card";
 import ActorCard from "../../components/actor-card";
 import moviesdata from "../../data/movies.json";
 import yobsdata from "../../data/yobs.json";
@@ -25,69 +26,80 @@ export default function MoviePage({ details, director, cast, yob }) {
   const [showCast, setShowCast] = useState(topCast);
 
   return (
-    <div class="my-6 py-6 mx-3">
-      <div class="narrow-container">
-        <div class="hero">
-          <div class="columns">
-            <div class="column is-narrow">
-              <MovieCard path={details.poster_path} />
-            </div>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta name="description" content={details.tagline} />
+        <meta property="og:title" content={details.title} key="ogtitle" />
+        <meta property="og:description" content={details.tagline} key="ogdesc" />
+        <meta property="og:image" content={getPath(details.poster_path)} key="ogimage" />
+        <title>{details.title}</title>
+      </Head>
+      <div class="my-6 py-6 mx-3">
+        <div class="narrow-container">
+          <div class="hero">
+            <div class="columns">
+              <div class="column is-narrow">
+                <MovieCard path={details.poster_path} />
+              </div>
 
-            <div class="column">
-              <div class="block">
-                <div class="content">
-                  <h1 class="title is-1 has-text-white">{details.title}</h1>
-                  <h1 class="subtitle is-3 has-text-primary">
-                    ({new Date(details.release_date).getFullYear()})
+              <div class="column">
+                <div class="block">
+                  <div class="content">
+                    <h1 class="title is-1 has-text-white">{details.title}</h1>
+                    <h1 class="subtitle is-3 has-text-primary">
+                      ({new Date(details.release_date).getFullYear()})
                   </h1>
-                </div>
-                <div class="block content is-medium">
-                  <p class="has-text-white subtitle is-3">{details.tagline}</p>
-                  <p class="has-text-white">
-                    {details.overview.replace(/&amp;/g, "&")}
+                  </div>
+                  <div class="block content is-medium">
+                    <p class="has-text-white subtitle is-3">{details.tagline}</p>
+                    <p class="has-text-white">
+                      {details.overview.replace(/&amp;/g, "&")}
+                    </p>
+                    <p class="title is-3 has-text-primary">
+                      {10 * details.vote_average}%
                   </p>
-                  <p class="title is-3 has-text-primary">
-                    {10 * details.vote_average}%
-                  </p>
-                  <p class="subtitle is-5 has-text-white">Viewer Rating</p>
-                </div>
+                    <p class="subtitle is-5 has-text-white">Viewer Rating</p>
+                  </div>
 
-                <Detail director={director} details={details} yob={yob} />
+                  <Detail director={director} details={details} yob={yob} />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div class="py-6 content">
-          <h1 class="title is-1 has-text-white">Top billed cast</h1>
+          <div class="py-6 content">
+            <h1 class="title is-1 has-text-white">Top billed cast</h1>
 
-          <div class="columns is-multiline is-mobile">
-            <>
-              {showCast.map((actor) => (
-                <div class="column is-one-fifth-tablet is-half-mobile">
-                  <ActorCard name={actor.name} subtitle={actor.character} image={actor.profile_path} type="actor" showSubtitle />
-                </div>
-              ))}
-            </>
-            {/* <MovieCard path={c.profile_path}/>))} */}
-          </div>
-          {showCast.length === 10 ? (
-            <button
-              className="button is-black"
-              onClick={() => setShowCast(cast)}
-            >
-              Show All
-            </button>
-          ) : (
+            <div class="columns is-multiline is-mobile">
+              <>
+                {showCast.map((actor) => (
+                  <div class="column is-one-fifth-tablet is-half-mobile">
+                    <ActorCard name={actor.name} subtitle={actor.character} image={actor.profile_path} type="actor" showSubtitle />
+                  </div>
+                ))}
+              </>
+              {/* <MovieCard path={c.profile_path}/>))} */}
+            </div>
+            {showCast.length === 10 ? (
               <button
                 className="button is-black"
-                onClick={() => setShowCast(topCast)}
+                onClick={() => setShowCast(cast)}
               >
-                Show Less
+                Show All
               </button>
-            )}
+            ) : (
+                <button
+                  className="button is-black"
+                  onClick={() => setShowCast(topCast)}
+                >
+                  Show Less
+                </button>
+              )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
