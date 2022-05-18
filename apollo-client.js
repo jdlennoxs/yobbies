52bf93008ee4c30d68f5a1f3393792a891af2022
Jack Scott <jack.scott10@gmail.com>
2022-05-18 18:36:38 +0100

    Removes serverless

--- apollo-client.js
+++ apollo-client.js
@@ -5,7 +5,7 @@ import config from "./src/aws-exports";
 const endpoint = config.aws_cloud_logic_custom[0];
 
 const client = new ApolloClient({
-  uri: endpoint,
+  uri: "http://localhost:3001/api/graphql",
   cache: new InMemoryCache(),
 });
 
